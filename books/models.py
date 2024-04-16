9c0016a2f168d20f905a4dd664cccf3c949fde3d
Abylaikhan Bariyev <abylaikhanbariev7@gmail.com>
2024-04-16 13:07:39 +0500

    Initial commit

---
+++ books/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    synopsis = models.TextField(blank=True, null=True)
+    publish_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, related_name='reviews', on_delete=models.CASCADE)
+    user = models.ForeignKey('auth.User', related_name='reviews', on_delete=models.CASCADE)
+    comment = models.TextField()
+    rating = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.rating} stars: {self.book.title} by {self.user.username}'
+
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    favorite_genre = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.user.username
