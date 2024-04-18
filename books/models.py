5a53e937fa476d5fce2e7e309cf5c076409450e2
Abylaikhan Bariyev <abylaikhanbariev7@gmail.com>
2024-04-18 20:01:58 +0500

    Minor update

--- books/models.py
+++ books/models.py
@@ -5,7 +5,6 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     synopsis = models.TextField(blank=True, null=True)
-    publish_date = models.DateField()
 
     def __str__(self):
         return self.title
